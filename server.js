fb4c2db4cdfd1f11a22a019875a811f76b6f87a6
Marianne Rachid <marianne.rachid@gmail.com>
2018-10-11 07:30:10 +0100

    create mongo collection

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+var db
+MongoClient.connect('mongodb://starwars:abcdef1@ds225703.mlab.com:25703/star-wars-quote',{ useNewUrlParser: true },  (err, client) => { 
+  if (err) return console.log(err)
+  db = client.db('star-wars-quote')
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
 
 app.use(bodyParser.urlencoded({extended: true}))
-
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
